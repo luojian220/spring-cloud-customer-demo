44d543e01f3cf52da5730f65f09980e1083957b0
罗剑 <luojian220@qq.com>
2018-12-20 18:09:29 +0800

    优化配置

--- src/main/java/com/luno/softone/springcloudcustomerdemo/controller/HelloWorldController.java
+++ src/main/java/com/luno/softone/springcloudcustomerdemo/controller/HelloWorldController.java
@@ -4,6 +4,8 @@ import com.luno.softone.springcloudcustomerdemo.dto.HelloMessageDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -18,6 +20,8 @@ import org.springframework.web.client.RestTemplate;
  * @since JDK 1.8
  */
 @RestController
+//这里面的属性有可能会更新的，git中的配置中心变化的话就要刷新，没有这个注解内，配置就不能及时更新
+@RefreshScope
 public class HelloWorldController {
 
     private static final Logger log = LoggerFactory.getLogger(HelloWorldController.class);
@@ -25,9 +29,12 @@ public class HelloWorldController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Value("${name}")
+    private String name;
+
     @GetMapping("/")
     public String home() {
-        return "hello world";
+        return "hello world , customer : " + name;
     }
 
     @GetMapping("/message")
